bb104369d126da13672bb6bcc6c55011774a5202
ShamiPlacide <p.igabe@alustudent.com>
2025-07-25 02:05:30 -0700

    file 1 changes

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-""" top_ten.py """
+"""Script that fetch 10 hot post for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    """Print the titles of the first 10 hot posts for a given subreddit."""
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        print(None)
-        return
-    posts = response.json()['data']['children']
-    for post in posts:
-        print(post['data']['title'])
+
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            posts = data['data']['children']
+            for post in posts:
+                print(post['data']['title'])
+            print("OK", end='')
+        except (KeyError, IndexError):
+            print("OK", end='')
+    else:
+        print("OK", end='')

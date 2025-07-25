d4a42585b52b3fe4ef2f26d3c837dc95d6cbc240
ShamiPlacide <p.igabe@alustudent.com>
2025-07-25 02:11:52 -0700

    file 1 changes

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -2,21 +2,26 @@
 """Script that fetch 10 hot post for a given subreddit."""
 import requests
 
-
 def top_ten(subreddit):
     """Print the titles of the first 10 hot posts for a given subreddit."""
     headers = {'User-Agent': 'MyAPI/0.0.1'}
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
-
+    
     if response.status_code == 200:
         try:
             data = response.json()
             posts = data['data']['children']
             for post in posts:
                 print(post['data']['title'])
-            print("OK", end='')
         except (KeyError, IndexError):
-            print("OK", end='')
+            print("None")
+    elif response.status_code == 404:
+        print("None")
+    elif response.status_code == 403:
+        print("None")
+    elif response.status_code == 302:
+        # Reddit redirects for private/banned subreddits
+        print("None")
     else:
-        print("OK", end='')
+        print("None")
